a1e808e4cdf95bdba31cac5179e72b71d1fad395
빈현욱 <peter.bhin@h2ohospitality.io>
2025-07-10 15:01:22 +0900

    fix: 전체적으로 수정 진행

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,17 +9,34 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    AsyncStorage.getItem("webview-history").then((val) => {
+      setReady(true);
+    });
+  }, []);
 
   if (!loaded) {
     // Async font loading only occurs in development.
     return null;
   }
+
+  if (!ready)
+    return (
+      <View>
+        <Text>로딩 중 입니다.</Text>
+      </View>
+    );
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
--- hooks/stackJotai.ts
+++ hooks/stackJotai.ts
@@ -1,13 +1,11 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { atom } from "jotai";
+// atoms.ts
 import { atomWithStorage } from "jotai/utils";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const StackListAtom = atom<string[]>([]);
-
-export const stackAtom = atomWithStorage<string[]>("webview-history", [], {
+export const historyAtom = atomWithStorage<string[]>("webview-history", [], {
   getItem: async (key) => {
-    const json = await AsyncStorage.getItem(key);
-    return json ? JSON.parse(json) : [];
+    const item = await AsyncStorage.getItem(key);
+    return item ? JSON.parse(item) : [];
   },
   setItem: async (key, value) => {
     await AsyncStorage.setItem(key, JSON.stringify(value));
